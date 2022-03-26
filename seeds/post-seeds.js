1cbea3f6c2db76b748475f70569c2661c149793d
Veoprato <75872616+Veoprato@users.noreply.github.com>
2022-03-26 17:04:26 -0400

    removed seeds/ from .gitignore

---
+++ seeds/post-seeds.js
@@ -0,0 +1,109 @@
+const { Post } = require('../models');
+
+const postdata = [
+  {
+    title: 'Donec posuere metus vitae ipsum.',
+    post_text: 'Check out this cool article I found. https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    user_id: 10
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    post_text: 'Check out this cool article I found. https://nasa.gov/donec.json',
+    user_id: 8
+  },
+  {
+    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+    post_text: 'Check out this cool article I found. https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    user_id: 1
+  },
+  {
+    title: 'Nunc purus.',
+    post_text: 'Check out this cool article I found. http://desdev.cn/enim/blandit/mi.jpg',
+    user_id: 4
+  },
+  {
+    title: 'Pellentesque eget nunc.',
+    post_text: 'Check out this cool article I found. http://google.ca/nam/nulla/integer.aspx',
+    user_id: 7
+  },
+  {
+    title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    post_text: 'Check out this cool article I found. https://stanford.edu/consequat.png',
+    user_id: 4
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    post_text: 'Check out this cool article I found. http://edublogs.org/non/ligula/pellentesque.js',
+    user_id: 1
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    post_text: 'Check out this cool article I found. http://ucla.edu/consequat/nulla.html',
+    user_id: 1
+  },
+  {
+    title: 'Duis ac nibh.',
+    post_text: 'Check out this cool article I found. http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
+    user_id: 9
+  },
+  {
+    title: 'Curabitur at ipsum ac tellus semper interdum.',
+    post_text: 'Check out this cool article I found. https://reverbnation.com/ligula/sit.jpg',
+    user_id: 5
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    post_text: 'Check out this cool article I found. http://china.com.cn/lectus/vestibulum.json',
+    user_id: 3
+  },
+  {
+    title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    post_text: 'Check out this cool article I found. http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
+    user_id: 10
+  },
+  {
+    title: 'Donec dapibus.',
+    post_text: 'Check out this cool article I found. https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
+    user_id: 8
+  },
+  {
+    title: 'Nulla tellus.',
+    post_text: 'Check out this cool article I found. https://lycos.com/natoque/penatibus/et.html',
+    user_id: 3
+  },
+  {
+    title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
+    post_text: 'Check out this cool article I found. https://gmpg.org/lorem.jpg',
+    user_id: 3
+  },
+  {
+    title:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
+    post_text: 'Check out this cool article I found. https://paginegialle.it/mattis/egestas.jsp',
+    user_id: 7
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    post_text: 'Check out this cool article I found. http://wikia.com/turpis/eget.jpg',
+    user_id: 6
+  },
+  {
+    title: 'Etiam justo.',
+    post_text: 'Check out this cool article I found. https://shareasale.com/quis.json',
+    user_id: 4
+  },
+  {
+    title: 'Nulla ut erat id mauris vulputate elementum.',
+    post_text: 'Check out this cool article I found. http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
+    user_id: 6
+  },
+  {
+    title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
+    post_text: 'Check out this cool article I found. https://java.com/at/nibh/in.png',
+    user_id: 7
+  }
+];
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
